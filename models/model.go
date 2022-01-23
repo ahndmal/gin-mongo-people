d5b0bdca1a5cc69b254d0f789e63623e141292eb
Andrii Maliuta <quadr988@gmail.com>
2022-01-23 16:03:49 +0200

    init

--- models/model.go
+++ models/model.go
@@ -1,6 +1,21 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
 type Person struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Id          primitive.ObjectID `json:"_id" bson:"_id"`
+	IdN         int                `json:"id" bson:"id"`
+	Name        string             `json:"name" bson:"name"`
+	FullName    string             `json:"fullName" bson:"fullName"`
+	Email       string             `json:"email" bson:"email"`
+	Gender      string             `json:"gender" bson:"gender"`
+	Status      string             `json:"status" bson:"status"`
+	Age         int                `json:"age" bson:"age"`
+	HasChildren bool               `json:"hasChildren"`
+	Engaged     bool               `json:"engaged"`
+	CreatedAt   time.Time          `json:"createdAt"`
+	CountryId   string             `json:"countryId" bson:"countryId"`
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+GOlang Web services that expose REST API endpoints taking data from Cloud MongoDB database.
+
+ENV:
+- MONGO_URL - url to cloud MongoDB instance
+- PORT - port that web service runs on
+
+It is based on Gin (https://github.com/gin-gonic/gin) web framework and core Mongo GO Drivers.
+
